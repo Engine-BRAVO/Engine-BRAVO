1402cbaddff3323267640f2a4e2ad385cd5ba05b
AngelVillanueva5 <sd.villanueva@student.avans.nl>
2024-09-12 12:56:50 +0200

    start of project

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -2,13 +2,9 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_video.h>
 
-#include "Animation.h"
-#include "SpriteAtlas.h"
-
-const int MOVE_SPEED = 10;
+#include "Dot.h"
+#include "Particle.h"
 
 int initSDL(SDL_Window*& window, SDL_Renderer*& renderer) {
     // Initialize SDL
@@ -35,13 +31,16 @@ int initSDL(SDL_Window*& window, SDL_Renderer*& renderer) {
     return 0;
 }
 
-void deInitSDL(SDL_Window*& window, SDL_Renderer*& renderer) {
+
+void deInitSDL(SDL_Window*& window, SDL_Renderer*& renderer) 
+{
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
-int main(int argc, char* args[]) {
+int main(int argc, char* args[]) 
+{
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
 
@@ -49,69 +48,5 @@ int main(int argc, char* args[]) {
 
     bool quit = false;
     SDL_Event event;
-
-    // Variables for animation timing and sprite movement
-    Uint32 lastTime = 0;         // Time of the last frame change
-    int frame = 0;               // Current frame (sprite in the sprite sheet)
-    const int spriteWidth = 16;  // Width of each sprite
-    const int spriteHeight = 25; // Height of each sprite
-    const int frameCount = 6;    // Total number of frames in the sprite sheet
-
-    // SDL_Texture* spriteSheetTexture = loadTexture(renderer, "enter_the_gungeon_spritesheet.png");
-    SpriteAtlas spriteAtlas(renderer, "enter_the_gungeon_spritesheet.png");
-
-    SDL_Rect startOfAnimation;
-    startOfAnimation.x = 22;          // Move horizontally in the sprite sheet
-    startOfAnimation.y = 187;         // Keep the vertical position constant (you can change this for vertical movement)
-    startOfAnimation.w = spriteWidth; // The width of the sprite
-    startOfAnimation.h = spriteHeight; // The height of the sprite
-
-    Animation& animation = spriteAtlas.getAnimation(startOfAnimation, frameCount);
-
-    // Define the destination rect where the image will be drawn
-    SDL_Rect destRect;
-    destRect.x = 100;    // The x position on the screen
-    destRect.y = 100;    // The y position on the screen
-    destRect.w = 18 * 4; // The width of the drawn image (scaling)
-    destRect.h = 26 * 4; // The height of the drawn image (scaling)
-
-    while (!quit) {
-        // Event handling
-        while (SDL_PollEvent(&event) != 0) {
-            if (event.type == SDL_QUIT) {
-                quit = true;
-            }
-            if (event.type == SDL_KEYDOWN) {
-                switch (event.key.keysym.sym) {
-                case SDLK_w: // Move up
-                    destRect.y -= MOVE_SPEED;
-                    break;
-                case SDLK_s: // Move down
-                    destRect.y += MOVE_SPEED;
-                    break;
-                case SDLK_a: // Move left
-                    destRect.x -= MOVE_SPEED;
-                    break;
-                case SDLK_d: // Move right
-                    destRect.x += MOVE_SPEED;
-                    break;
-                }
-            }
-        }
-        // Clear screen
-        SDL_RenderClear(renderer);
-
-        SDL_Rect srcRect = animation.getCurrentFrame();
-        SDL_Texture* spriteTexture = animation.getTexture();
-
-        // Copy part of the sprite sheet to the renderer
-        SDL_RenderCopy(renderer, spriteTexture, &srcRect, &destRect);
-
-        // Update the screen
-        SDL_RenderPresent(renderer);
-    }
-
-    // Clean up
-    deInitSDL(window, renderer);
-    return 0;
 }
+
