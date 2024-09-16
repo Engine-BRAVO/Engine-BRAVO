768f5c08af7323f169c1a9cdf23d8e6e35afc443
Ronan Vlak <71377825+RonanVlak@users.noreply.github.com>
2024-09-16 11:44:41 +0200

    Edited tinytmx_install.sh

--- code/install/TinyTmx_install.sh
+++ code/install/TinyTmx_install.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
+
 # Install Zlib
 sudo apt-get install zlib1g-dev -y
 
 # Install TinyXML2
-sudo apt-get install libtinyxml2-dev -y
+#sudo apt-get install libtinyxml2-dev -y
+# Remove any existing TinyXML2 version
+#sudo apt-get remove libtinyxml2-dev -y
+
+cd ../../..
+# Download and install specific version of TinyXML2 (9.0.0)
+wget https://github.com/leethomason/tinyxml2/archive/refs/tags/9.0.0.tar.gz
+tar -xzvf 9.0.0.tar.gz 
+cd tinyxml2-9.0.0
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
 
 # Install Zstd
-sudo apt-get install libzstd-dev -y
+# Remove any existing Zstd version
+#sudo apt-get remove libzstd-dev -y
+
+cd ../..
+# Download and install specific version of Zstd (1.5.2)
+wget https://github.com/facebook/zstd/archive/refs/tags/v1.5.2.tar.gz 
+tar -xzvf v1.5.2.tar.gz
+cd zstd-1.5.2
+make
+sudo make install
+
 
