92249669af5e9f4ade53349fdba08a8af69fc1a2
Ronan Vlak <71377825+RonanVlak@users.noreply.github.com>
2024-09-16 12:38:29 +0200

    Test cmakelist

--- code/install/TinyTmx_install.sh
+++ code/install/TinyTmx_install.sh
@@ -8,7 +8,9 @@ sudo apt-get install zlib1g-dev -y
 # Remove any existing TinyXML2 version
 #sudo apt-get remove libtinyxml2-dev -y
 
-cd ../../..
+cd ..
+mkdir tinyxml2
+cd tinyxml2
 # Download and install specific version of TinyXML2 (9.0.0)
 wget https://github.com/leethomason/tinyxml2/archive/refs/tags/9.0.0.tar.gz
 tar -xzvf 9.0.0.tar.gz 
@@ -16,19 +18,25 @@ cd tinyxml2-9.0.0
 mkdir build
 cd build
 cmake ..
-make
+make -j 16
 sudo make install
 
 # Install Zstd
 # Remove any existing Zstd version
 #sudo apt-get remove libzstd-dev -y
 
-cd ../..
+cd ../../..
+mkdir zlib
+cd zlib
 # Download and install specific version of Zstd (1.5.2)
 wget https://github.com/facebook/zstd/archive/refs/tags/v1.5.2.tar.gz 
 tar -xzvf v1.5.2.tar.gz
 cd zstd-1.5.2
-make
+make -j 16
 sudo make install
+cd build/cmake
+mkdir build
+cd build
+cmake ..
 
 
