6771e544407d0b468c49a03d047d89577db9143e
heavydragon99 <heavydragon99@outlook.com>
2024-10-02 14:57:32 +0200

    commit to save data

--- code/inc/ai.h
+++ code/inc/ai.h
@@ -1,4 +1,32 @@
-// #ifndef AI_H
-// #define AI_H
+#ifndef AI_H
+#define AI_H
 
-// #endif // AI_H
+#include "maze.h"
+
+#include <vector>
+#include <unordered_map>
+
+class ai
+{
+public:
+    ai(maze &aMaze);
+
+private:
+    maze &mMaze;
+
+    struct Node
+    {
+        int x;
+        int y;
+        int gCost;
+        int hCost;
+        int fCost;
+        Node *parent;
+    };
+
+    std::vector<std::pair<int, int>> mPath;
+    std::unordered_map<int, Node *> mOpenSet;
+    std::unordered_map<int, Node *> mClosedSet;
+};
+
+#endif // AI_H
